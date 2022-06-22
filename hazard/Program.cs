09af3a1cb09566331d0fbcc9c1634f9c3817cd48
wikii199 <ania.welna7@gmail.com>
2022-06-22 18:52:03 +0200

    Dodaj pliki projektów.

---
+++ hazard/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace hazard
+{
+class program
+    {
+        static void Main(string[] args) 
+        {
+            double odds = .75;
+            Random random = new Random();
+            guy player = new guy() { Cash = 500, Name = "gracz" };
+
+            Console.WriteLine("siema witam prawododonienstwo ze wygrasz;" + odds);
+            while (player.Cash >0) 
+            {
+                player.info();
+                Console.WriteLine(" ile obstawiasz chuju:");
+                string ile = Console.ReadLine();
+                if (int.TryParse(ile, out int amout )) 
+                {
+                    int bet = player.dajhajs(amout) * 2;
+                    if (bet > 0) 
+                    {
+                        if (random.NextDouble() > odds) 
+                        {
+                            int win = bet; 
+                            Console.WriteLine(" wygrales " + win);
+                            player.dawajhajshuju(win);
+                           
+                        }
+                        else 
+                        {
+                            Console.WriteLine(" giga przegryw ");
+
+                        }
+                    }
+                }   else
+                {
+                    Console.WriteLine(" dawaj liczbe ");
+
+                }
+            }
+            Console.WriteLine("chuj ci bo kasyno wygrywa ");
+        }
+        
+    }
+}
---
+++ hazard/guy.cs
@@ -0,0 +1,43 @@
+﻿
+    class guy
+    {
+        public string Name;
+        public int Cash;
+
+        public void info()
+        {
+            Console.WriteLine(Name + " ma " + Cash + " hajsu. ");
+
+        }
+        public int dajhajs(int amout)
+        {
+            if (amout <= 0)
+            {
+                Console.WriteLine(Name + " mowi" + amout + "jest chujowa kwota");
+                return 0;
+            }
+            if (amout > Cash)
+            {
+                Console.WriteLine(Name + "mowi: " + " nie mam hajsu na tyle kurwo" + amout);
+                return 0;
+
+            }
+            Cash -= amout;
+            return amout;
+        }
+        public void dawajhajshuju(int amout)
+        {
+            if (amout <= 0)
+            {
+                Console.WriteLine(Name + "mowi" + amout + " to za malo chuju");
+
+            }
+            else
+            {
+                Cash += amout;
+
+            }
+        }
+
+    }
+
